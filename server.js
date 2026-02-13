7697d59f106d0280638943d973f9c242c3dfcf8c
KaydenWethmar <131465331+TourSouthAfrica@users.noreply.github.com>
2026-02-13 16:09:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -392,9 +392,10 @@ function mergeUsersIntoPrimary(db, primaryId, dupeIds) {
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>
-  console.log(`Server running at http://localhost:${PORT}`)
-);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 
 
 app.get("/classes", (req, res) => {
