fbffd2575c31fb0c7a7287f572abc213839d1ce4
KaydenWethmar <131465331+TourSouthAfrica@users.noreply.github.com>
2026-02-13 16:06:12 +0200

    Delete check_users_schema.js

--- check_users_schema.js
+++
@@ -1,6 +0,0 @@
-const Database = require("better-sqlite3");
-const path = require("path");
-const db = new Database(path.join(__dirname, "data.sqlite"));
-
-console.log("DB file:", path.join(__dirname, "data.sqlite"));
-console.log(db.prepare("PRAGMA table_info(users)").all());
